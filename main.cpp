e143b59d576b2ff1141b7b65c59befe6ceb2a38a
Grey-cat-cat <kiril_fom27@mail.ru>
2026-03-13 19:57:51 +0300

    s

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#pragma once
+#include "calc.h"
+
+int main() {
+  int input = 0;
+  int a = 0;
+  int b = 0;
+
+  while(true) {
+    std::cout <<"input 0 to end\n" << "input 1 to add\n" << "input 2 to multiplicate\n" <<"input 3 to divide\n" << "input 4 to subtr\n";
+    std::cin >> input;
+    if ((input <= 0) || (input > 4)) {
+      break;
+    }
+    if (input == 1) {
+      std::cout << "A - B  Input A, and B\n";
+      std::cin >> b >> a;
+      std::cout << Add(a, b) << "\n";
+    }
+    if (input == 2) {
+      std::cout << "A * B  Input A, and B\n";
+      std::cin >> a >> b;
+      std::cout << Mult(a, b) << "\n";
+    }
+    if (input == 3) {
+      std::cout << "A / B  Input A, and B\n";
+      std::cin >> a >> b;
+      std::cout << Div(a, b) << "\n";
+    }
+    if (input == 4) {
+      std::cout << "A - B  Input A, and B\n";
+      std::cin >> a >> b;
+      std::cout << Sub(a, b) << "\n";
+    }
+  }
+  std::cout << "end\n";
+  return 0;
+}
+
